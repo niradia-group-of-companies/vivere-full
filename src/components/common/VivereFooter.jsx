5a3827b16ea2fa8afc2193ca2b0d9489f3dac7b1
billc <billc@niradia.com>
2024-06-21 16:09:15 -0700

    finish creating mockup for footer

--- src/components/common/VivereFooter.jsx
+++ src/components/common/VivereFooter.jsx
@@ -1,8 +1,99 @@
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaInstagram,
+  FaYoutube,
+} from "react-icons/fa6";
+
 export const VivereFooter = () => {
   return (
-    <div className="bg-th_green bg-vivere bg-no-repeat bg-right-top md:bg-right-bottom bg-[length:auto_30%] md:bg-[length:auto_90%] w-full h-fit lg:h-[340px] pb-8 md:pb-0">
-      <div className="w-11/12 py-4 h-full mx-auto flex flex-col-reverse md:flex-row items-start md:items-center justify-start gap-2 md:gap-8 2xl:gap-16">
-        <div className="mt-52 md:mt-0 text-th_ivory h-full w-auto flex flex-col justify-end items-start pb-0 lg:pb-5"></div>
+    <div className="bg-th_green text-th_ivory p-8">
+      <div></div>
+      <div className="flex">
+        <div className="basis-1/5 px-20">
+          <img
+            src="/logos/Vivere Secondary - White.png"
+            alt="leaf_background_image"
+            className="h-[220px] object-contain"
+          />
+        </div>
+        <div className="basis-4/5 flex justify-around">
+          <ul className="flex flex-col gap-2 text-xl p-12">
+            <li className="border-b-2 pb-1 w-48">LOCATION</li>
+            <li className="border-b-2 pb-1 w-48">INTERIORS</li>
+            <li className="border-b-2 pb-1 w-48">EXTERIORS</li>
+            <li className="border-b-2 pb-1 w-48">FLOORPLANS</li>
+            <li className="border-b-2 pb-1 w-48">AMENITIES</li>
+            <li className="border-b-2 pb-1 w-48">DEVELOPER</li>
+          </ul>
+          <div className="flex flex-col gap-2 text-xl p-12">
+            <div className="pb-5">
+              <h4 className="pb-5">Presentation Center</h4>
+              <p>
+                1545 Johnston Road, <br />
+                White Rock, BC Canada
+                <br /> V4B 3Z6
+              </p>
+            </div>
+            <div className="pb-10">
+              <p>
+                Open Daily, 12-5pm <br />
+                <i>Except Fridays</i>
+              </p>
+            </div>
+            <div>
+              <p>604.531.8238</p>
+              <p>info@viverebysolterra.ca</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 text-xl p-12 justify-between">
+            <div className="relative right-10">
+              <h4 className="pb-5">Vivere Location</h4>
+              <p>
+                15202 Guildford Drive, <br />
+                Surrey, BC Canada
+                <br /> V3R 0H9
+              </p>
+            </div>
+            <div>
+              <div className="flex text-th_brown text-4xl justify-around items-center pb-4 gap-3">
+                <a
+                  href="https://facebook.com/solterradev"
+                  className=""
+                  target="_blank"
+                >
+                  <FaFacebook />
+                </a>
+                <a
+                  href="https://instagram.com/solterradev/"
+                  className=""
+                  target="_blank"
+                >
+                  <FaInstagram />
+                </a>
+                <a
+                  href="https://linkedin.com/company/solterradevelopmentcorp/"
+                  className=""
+                  target="_blank"
+                >
+                  <FaLinkedin />
+                </a>
+                <a
+                  href="https://youtube.com/solterradevelopmentcorp"
+                  className=""
+                  target="_blank"
+                >
+                  <FaYoutube />
+                </a>
+              </div>
+              <img
+                className=""
+                src="/logos/solterra_footer_logo.svg"
+                alt="solterra_logo"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
