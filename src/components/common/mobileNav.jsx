c9677211fa8503d5223ab759555ccca2a04438ea
billc <billc@niradia.com>
2024-06-24 13:25:52 -0700

    make the mobile navigation fixed

--- src/components/common/mobileNav.jsx
+++ src/components/common/mobileNav.jsx
@@ -15,7 +15,7 @@ export const MobileNav = () => {
   return (
     <>
       <div
-        className="absolute text-th_green top-0 right-0 p-7 z-[999] xl:hidden"
+        className="fixed text-th_green top-0 right-0 p-7 z-[999] xl:hidden"
         onClick={toggleHamburger}
       >
         <TfiAlignRight size={50} />
@@ -27,13 +27,13 @@ export const MobileNav = () => {
         }
       >
         <div
-          className="absolute text-th_ivory p-7 top-0 right-0 z-[1000]"
+          className="fixed text-th_ivory p-7 top-0 right-0 z-[1001]"
           onClick={toggleHamburger}
         >
           <TfiClose size={50} />
         </div>
 
-        <ul class="text-3xl font-semibold gap-5 flex flex-col text-th_ivory w-screen h-screen bg-th_green justify-center items-center">
+        <ul class="fixed text-3xl font-semibold gap-5 flex flex-col text-th_ivory w-screen h-screen bg-th_green justify-center items-center z-[1000]">
           <li>LOCATION</li>
           <li>INTERIORS</li>
           <li>EXTERIORS</li>
